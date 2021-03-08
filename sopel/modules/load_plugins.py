2e4f918fb28b9d8af1b637cede9d5492a8d3f730
Sickmantella <73144459+Sickmantella@users.noreply.github.com>
2021-03-08 09:40:49 -0700

    Update load_plugins.py

--- sopel/modules/load_plugins.py
+++ sopel/modules/load_plugins.py
@@ -21,7 +21,7 @@ class db:
             global conn
             global cur
             conn = mariadb.connect(
-                user="ScoutLink", #Maybe don't put that word on here (Cause if council see it)
+                user="Not_Here", #Maybe don't put that word on here (Cause if council see it)
                 password="N0T_H3R3!",
                 host="127.0.0.1",
                 port=3306,
